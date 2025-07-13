297f5939ab6a63daa3b73ea7c2ab1276cc0f6d7c
TrongPhuc <nguyentrongphuc03@gmail.com>
2025-07-13 14:54:13 +0700

    chat-image

--- src/main/java/ntp/springaifirst/controller/ChatController.java
+++ src/main/java/ntp/springaifirst/controller/ChatController.java
@@ -4,6 +4,7 @@ import ntp.springaifirst.dto.ChatRequest;
 import ntp.springaifirst.service.ChatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class ChatController {
@@ -13,5 +14,11 @@ public class ChatController {
     @PostMapping("/chat")
     String chat(@RequestBody ChatRequest request) {
         return chatService.chat(request);
+    }
+
+    @PostMapping("/chat-image")
+    String chatImage(@RequestParam("file") MultipartFile file,
+            @RequestParam("message") String message) {
+        return chatService.chatImage(file, message);
     }
 }
--- src/main/java/ntp/springaifirst/service/ChatService.java
+++ src/main/java/ntp/springaifirst/service/ChatService.java
@@ -1,13 +1,17 @@
 package ntp.springaifirst.service;
 
 import ntp.springaifirst.dto.ChatRequest;
+import org.apache.tomcat.util.http.fileupload.util.mime.MimeUtility;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.messages.SystemMessage;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.content.Media;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class ChatService {
@@ -26,6 +30,20 @@ public class ChatService {
 
         return chatClient
                 .prompt(prompt)
+                .call()
+                .content();
+    }
+
+    public String chatImage(MultipartFile file, String message) {
+        Media media = Media.builder()
+                .mimeType(MimeTypeUtils.parseMimeType(file.getContentType()))
+                .data(file.getResource())
+                .build();
+
+        return chatClient.prompt()
+                .system("You are Phucabcd backend developer")
+                .user(promptUserSpec -> promptUserSpec.media(media)
+                .text(message))
                 .call()
                 .content();
     }
